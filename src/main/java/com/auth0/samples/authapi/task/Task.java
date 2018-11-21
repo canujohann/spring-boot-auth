38fb16ecf93ff5a93fcb903bc16fe780738ae815
Johann Canu <canu.johann@gmail.com>
2018-11-21 10:46:44 +0900

    add Lombok library

--- src/main/java/com/auth0/samples/authapi/task/Task.java
+++ src/main/java/com/auth0/samples/authapi/task/Task.java
@@ -4,29 +4,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Task {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
 	private long id;
+
+	@Getter
+	@Setter
 	private String description;
 
 	protected Task() { }
 
 	public Task(String description) {
-		this.description = description;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
 		this.description = description;
 	}
 }
--- src/main/java/com/auth0/samples/authapi/user/ApplicationUser.java
+++ src/main/java/com/auth0/samples/authapi/user/ApplicationUser.java
@@ -4,32 +4,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class ApplicationUser {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
 	private long id;
+
+	@Getter
+	@Setter
 	private String username;
+
+	@Getter
+	@Setter
 	private String password;
 
-	public long getId() {
-		return id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
